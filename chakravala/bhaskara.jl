d92c841767103a3e7788422f4da3c15c6d6fbc12
Christopher D. Long <galizur@gmail.com>
2014-09-29 16:04:15 -0400

    Now handles +/-2, +/- 4 cases.

--- chakravala/bhaskara.jl
+++ chakravala/bhaskara.jl
@@ -30,70 +30,61 @@ m = m_init
 
 println("[h, m] = [$h, $m]")
 
-#while !(h in [BigInt(1),BigInt(2),BigInt(4)])
-while !(h==1)
+while !(h==1 || h==2 || h==4)
 
     x = div((m_init*x_init+y_init),h_init)
     y = div((D*x_init+m_init*y_init),h_init)
 
-    #println(D-m_init^2, " ", h_init, " ", div((D-m_init^2),h_init))
     h = div((D-m_init^2),h_init)
-
-    #println(-y," ",x," ",h," ",mod(-y,h)," ",invmod(x,h))
-    #i = mod(div(-y,x),h)
     i = mod(mod(-y,h)*invmod(x,h),h)
     
     m = h*div(sD,h)+i
-    #println("[x, y, h, i, m] = [$x, $y, $h, $i, $m]")
 
     if (m>sD)
         m += -h
     end
 
-    #println("m = $m")
-
-    x_init = x
-    y_init = y
-    h_init = h
-    m_init = m
+    (x_init, y_init, h_init, m_init) = (x, y, h, m)
     steps += 1
 
     println("[h, m] = [$h, $m]")
 
 end
 
-if (steps % 2)==0
-    a = x
-    b = y
-    x = 2*a*b
-    y = D*a^2+b^2
+if (h==1) && (steps % 2)==0
+    
+    (x, y) = (2*x*y, D*x^2+y^2)
+    
+elseif (h==2)
+
+    # Separated to illustrate the algorithmic steps
+    
+    (x, y) = (2*x*y, D*x^2+y^2)
+    (x, y) = (div(x,2), div(y,2))
+    
+elseif (h==4)
+
+    # Need to use rational arithmetic to handle certain cases
+    
+    (a, b) = (x//2, y//2)
+    
+    if (isinteger(a) && isinteger(b))
+        (x, y) = (num(a), num(b))
+    else
+        (a_1, b_1) = (2*a*b, D*a^2+b^2)
+
+        if (isinteger(a_1) && isinteger(b_1))
+            (x, y) = (num(a_1), num(b_1))
+        else
+            (x, y) = (num(a*b_1+a_1*b), num(b*b_1+D*a*a_1))
+            if (steps % 2)==0
+                (x,y) = (2*x*y, D*x^2+y^2)
+            end
+        end
+    end
+
 end
 
 v = y^2-D*x^2
 println("Solution = [$x, $y, $v]")
 println("Steps = $steps")
-
-#if (h==2):
-#    (x,y) = (2*x*y, D*x**2+y**2)
-#    (x,y) = (x/2, y/2)
-#elif (h==4):
-#    (a, b) = (Rational(x)/2, Rational(y)/2)
-#    print(a,b)
-#    if (a.is_integral() & b.is_integral()):
-#        (x,y) = (a,b)
-#    else:
-#        (a_1, b_1) = (2*a*b, D*a**2+b**2)
-#        print(a_1,b_1)
-#        if (a_1.is_integral() & b_1.is_integral()):
-#            (x,y) = (a_1,b_1)
-#        else:
-#            (x, y) = (a*b_1+a_1*b, b*b_1+D*a*a_1)
-#            if (steps % 2)==0:
-#                (x,y) = (2*x*y, D*x**2+y**2)
-#else:
-#    if (steps % 2)==0:
-#        (x,y) = (2*x*y, D*x**2+y**2)
-
-#v = y**2-D*x**2
-#print("Solution = [%s, %s, %s]" % (x, y, v))
-#print("Steps = %s" % steps)
