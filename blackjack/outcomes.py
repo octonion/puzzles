46c041960b0d912869da9b8f15f57ccb3b30967e
Christopher D. Long <galizur@gmail.com>
2017-10-16 17:00:18 -0400

    Blackjack, infinite shoe, no splitting

---
+++ blackjack/outcomes.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+deck = ([4]*9)
+deck.append(16)
+
+def partitions(cards, subtotal):
+
+    m = 0
+    # Hit
+    for i in range(0, 10):
+        if (cards[i]>0):
+            
+            cards[i] = cards[i]-1
+            total = subtotal+i+1
+            
+            if (total < 21):
+                
+                # Stand
+                m += 1
+                #print(cards, total)
+                # Hit again
+                m += partitions(cards, total)
+                
+            elif (subtotal+i+1==21):
+                
+                # Stand; hit again is an automatic bust
+                m += 1
+                #print(cards, total)
+                
+            cards[i] = cards[i]+1
+                
+    return(m)
+
+d = 0
+
+for i in range(0, 10):
+
+    # Dealer showing
+
+    deck[i] = deck[i]-1
+
+    p = 0
+    for j in range(0, 10):
+        deck[j] = deck[j]-1
+        n = partitions(deck, j+1)
+        #print('Starting with ', j, n)
+        deck[j] = deck[j]+1
+        p += n
+
+    print('Dealer showing ', i,' partitions =',p)
+    d += p
+
+    deck[i] = deck[i]+1
+
+print('Total partitions =',d)
