046a3b01a0a04a9ccfbb3f2152160468e0a8ae80
Christopher D. Long <galizur@gmail.com>
2017-10-26 17:28:29 -0400

    Outcome calculations in D

---
+++ blackjack/dlang/outcomes.d
@@ -0,0 +1,51 @@
+void main()
+{
+    import std.stdio: write, writeln, writef, writefln;
+    
+    int[10] deck = [4,4,4,4,4,4,4,4,4,16];
+
+    int partitions(int[10] cards, int subtotal)
+    {
+      //writeln(cards,subtotal);
+      int m=0;
+      int total;
+      // Hit
+      for (int i = 0; i < 10; i++) {
+	if (cards[i]>0) {
+	  cards[i] = cards[i]-1;
+	  total = subtotal+i+1;
+	  if (total < 21) {
+	    // Stand
+	    m += 1;
+	    // Hit again
+	    m += partitions(cards, total);
+	  } else if (subtotal+i+1==21) {
+	    // Stand; hit again is an automatic bust
+	    m += 1;
+	  }
+	  cards[i] = cards[i]+1;
+	}
+      }
+      return m;
+    }
+
+    int d=0;
+    for (int i = 0; i < 10; i++) {
+      // Dealer showing
+      deck[i] = deck[i]-1;
+
+      int p = 0;
+      for (int j = 0; j < 10; j++) {
+	deck[j] = deck[j]-1;
+	int n = partitions(deck, j+1);
+	deck[j] = deck[j]+1;
+	p += n;
+      }
+
+      writeln("Dealer showing ",i," partitions =",p);
+      d += p;
+      deck[i] = deck[i]+1;
+    }
+
+    writeln("Total partitions =",d);
+}
