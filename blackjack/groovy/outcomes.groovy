a0b17852b8d58fd9aff71f0475e00fde26ad3d88
Christopher D. Long <galizur@gmail.com>
2017-12-12 02:07:08 -0500

    Blackjack outcome partitions in Groovy.

---
+++ blackjack/groovy/outcomes.groovy
@@ -0,0 +1,63 @@
+// Groovy
+
+def deck = [4]*9
+deck << new Integer(16)
+
+def partitions
+
+partitions = { ArrayList cards, int subtotal ->
+
+    def m = 0
+    // Hit
+    for (i in 0..9) {
+        if (cards[i]>0) {
+
+            cards[i] = cards[i]-1
+            total = subtotal+i+1
+            
+            if (total < 21) {
+                
+                // Stand
+                m += 1
+
+		// Hit again
+                m += partitions(cards, total)
+                
+	    } else if (subtotal+i+1==21) {
+		
+                // Stand; hit again is an automatic bust
+                m += 1
+		
+	    }
+                
+            cards[i] = cards[i]+1
+
+	}
+    }
+                
+    return m
+}
+
+def d = 0
+
+for (i in 0..9) {
+
+    // Dealer showing
+
+    deck[i] = deck[i]-1
+
+    def p = 0
+    for (j in 0..9) {
+        deck[j] = deck[j]-1
+        n = partitions(deck, j+1)
+        deck[j] = deck[j]+1
+        p += n
+    }
+
+    println "Dealer showing ${i} partitions = ${p}"
+    d += p
+
+    deck[i] = deck[i]+1
+}
+
+println "Total partitions = ${d}"
