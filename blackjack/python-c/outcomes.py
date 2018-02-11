c551709a6903d1c7e0930675c5d8fcd456892abc
Christopher D. Long <galizur@gmail.com>
2018-02-10 23:38:42 -0500

    Python core.

---
+++ blackjack/python-c/outcomes.py
@@ -0,0 +1,35 @@
+from ctypes import *
+import os
+
+test_lib = cdll.LoadLibrary(os.path.abspath("libpartitions.so"))
+test_lib.partitions.argtypes = [POINTER(c_int), c_int]
+test_lib.partitions.restype = c_int
+
+deck = ([4]*9)
+deck.append(16)
+
+d = 0
+
+for i in range(0, 10):
+
+    # Dealer showing
+
+    deck[i] = deck[i]-1
+
+    p = 0
+    for j in range(0, 10):
+        deck[j] = deck[j]-1
+
+        nums_arr = (c_int*len(deck))(*deck)
+            
+        n = test_lib.partitions(nums_arr, c_int(j+1))
+        #print('Starting with ', j, n)
+        deck[j] = deck[j]+1
+        p += n
+
+    print('Dealer showing ', i,' partitions =',p)
+    d += p
+
+    deck[i] = deck[i]+1
+
+print('Total partitions =',d)
