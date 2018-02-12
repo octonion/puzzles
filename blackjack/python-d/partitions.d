0694c8a0e074d7d554875e2d4c8961a7a74ecf4e
Christopher D. Long <galizur@gmail.com>
2018-02-12 17:51:27 -0500

    Calling D from Python. Currently broken.

---
+++ blackjack/python-d/partitions.d
@@ -0,0 +1,27 @@
+import std.stdio: write, writeln, writef, writefln;
+
+extern (C) int partitions(int[10] cards, int subtotal)
+{
+  writeln(cards);
+  writeln(subtotal);
+  int m=0;
+  int total;
+  // Hit
+  for (int i = 0; i < 10; i++) {
+    if (cards[i]>0) {
+      cards[i] = cards[i]-1;
+      total = subtotal+i+1;
+      if (total < 21) {
+	// Stand
+	m += 1;
+	// Hit again
+	m += partitions(cards, total);
+      } else if (subtotal+i+1==21) {
+	// Stand; hit again is an automatic bust
+	m += 1;
+      }
+      cards[i] = cards[i]+1;
+    }
+  }
+  return m;
+}
