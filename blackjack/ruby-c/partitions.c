81b929bffd61b8e6a32892941ad281c37daf4e82
Christopher D. Long <galizur@gmail.com>
2018-02-17 00:08:10 -0500

    Calling C from Ruby.

---
+++ blackjack/ruby-c/partitions.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+    int partitions(int cards[10], int subtotal)
+    {
+      //writeln(cards,subtotal);
+      int m=0;
+      int total;
+      // Hit
+      for (int i = 0; i < 10; i++) {
+	if (cards[i]>0) {
+	  cards[i] = cards[i]-1;
+	  total = subtotal+i+1;
+	  if (total < 21) {
+	    // Stand
+	    m += 1;
+	    // Hit again
+	    m += partitions(cards, total);
+	  } else if (subtotal+i+1==21) {
+	    // Stand; hit again is an automatic bust
+	    m += 1;
+	  }
+	  cards[i] = cards[i]+1;
+	}
+      }
+      return m;
+    }
