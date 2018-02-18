9b69b18912c0ae153bd8cc51dfdb5cb9c2a109f8
Christopher D. Long <galizur@gmail.com>
2018-02-18 14:58:44 -0500

    Blackjack outcome partitions in Javascript.

---
+++ blackjack/javascript/outcomes.js
@@ -0,0 +1,49 @@
+function partitions(cards, subtotal) {
+    var m=0;
+    var total;
+    //console.log(cards);
+    // Hit
+    for (var i = 0; i < 10; i++) {
+	if (cards[i]>0) {
+	    cards[i] += -1;
+	    total = subtotal+i+1;
+	    if (total < 21) {
+		// Stand
+		m += 1;
+		// Hit again
+		m += partitions(cards, total);
+	    } else if (total==21) {
+		// Stand; hit again is an automatic bust
+		m += 1;
+	    }
+	    cards[i] += 1;
+	}
+    }
+    return m;
+}
+
+function main() {
+
+    var deck = new Array(4,4,4,4,4,4,4,4,4,16);
+    var d=0;
+  
+    for (var i = 0; i < 10; i++) {
+	// Dealer showing
+	deck[i] += -1;
+	var p = 0;
+	for (var j = 0; j < 10; j++) {
+	    deck[j] += -1;
+	    //console.log(deck);
+	    var n = partitions(deck, j+1);
+	    deck[j] += 1;
+	    p += n;
+	}
+
+	console.log("Dealer showing " + i + " partitions = " + p);
+	d += p;
+	deck[i] += 1;
+    }
+    console.log("Total partitions = " + d);
+}
+
+main();
