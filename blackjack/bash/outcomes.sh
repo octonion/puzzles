b8c44e7a0eca13958ffc3ea44f974b2211b211d5
Christopher D. Long <galizur@gmail.com>
2018-02-19 21:39:26 -0500

    Blackjack outcome partitions in bash.

---
+++ blackjack/bash/outcomes.sh
@@ -0,0 +1,49 @@
+partitions() {
+    local i
+    #echo ${cards[@]}
+
+    # Hit
+    for (( i=0; (( i<10 )); $((i++)) )); do
+	if (( cards[i]>0 )); then
+	    (( cards[i]+=-1 ))
+	    (( total=total+i+1 ))
+	    if (( total<21 )); then
+		# Stand
+		(( m=m+1 ))
+		# Hit again
+		partitions
+	    elif (( total==21 )); then
+		# Stand; hit again is an automatic bust
+		(( m=m+1 ))
+	    fi
+	    (( total=total-i-1 ))
+	    (( cards[i]+=1 ))
+	fi
+    done
+}
+
+declare -a cards=( 4 4 4 4 4 4 4 4 4 16 )
+
+d=0
+  
+for (( i=0; (( i<10 )); $((i++)) )); do
+    # Dealer showing
+    (( cards[i]+=-1 ))
+    p=0
+    #echo ${cards[@]}
+    for (( j=0; (( j<10 )); $((j++)) )); do
+	(( cards[j]+=-1 ))
+	(( total=j+1 ))
+	(( m=0 ))
+	partitions
+	(( p=p+m ))
+	(( cards[j]+=1 ))
+    done
+    #echo ${cards[@]}
+
+    echo "Dealer showing ${i} partitions = ${p}"
+    (( d=d+p ))
+    (( cards[i]+=1 ))
+done
+
+echo "Total partitions = ${d}"
