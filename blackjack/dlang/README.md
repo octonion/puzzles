f6cdd8fbfc07f7f4adc114760d9934ff150aefdd
Christopher D. Long <galizur@gmail.com>
2018-03-07 21:26:16 -0500

    D code improvements.

---
+++ blackjack/dlang/README.md
@@ -0,0 +1,14 @@
+```bash
+rdmd --compiler=ldc2 -O3 -boundscheck=off outcomes.d
+```
+
+```
+dmd -O outcomes.d
+./outcomes
+```
+
+```bash
+ldc2 -O3 outcomes.d
+./outcomes
+```
+
--- blackjack/dlang/outcomes.d
+++ blackjack/dlang/outcomes.d
@@ -12,18 +12,18 @@ void main()
       // Hit
       for (int i = 0; i < 10; i++) {
 	if (cards[i]>0) {
-	  cards[i] = cards[i]-1;
-	  total = subtotal+i+1;
+	  	  total = subtotal+i+1;
 	  if (total < 21) {
+	    cards[i] -= 1;
 	    // Stand
 	    m += 1;
 	    // Hit again
 	    m += partitions(cards, total);
+	    cards[i] += 1;
 	  } else if (subtotal+i+1==21) {
 	    // Stand; hit again is an automatic bust
 	    m += 1;
 	  }
-	  cards[i] = cards[i]+1;
 	}
       }
       return m;
@@ -32,19 +32,19 @@ void main()
     int d=0;
     for (int i = 0; i < 10; i++) {
       // Dealer showing
-      deck[i] = deck[i]-1;
+      deck[i] -= 1;
 
       int p = 0;
       for (int j = 0; j < 10; j++) {
-	deck[j] = deck[j]-1;
+	deck[j] -= 1;
 	int n = partitions(deck, j+1);
-	deck[j] = deck[j]+1;
+	deck[j] += 1;
 	p += n;
       }
 
       writeln("Dealer showing ",i," partitions =",p);
       d += p;
-      deck[i] = deck[i]+1;
+      deck[i] += 1;
     }
 
     writeln("Total partitions =",d);
