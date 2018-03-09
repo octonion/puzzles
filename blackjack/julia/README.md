a5552974b725cdb725505bc6f9a872e40b32adb3
Christopher D. Long <galizur@gmail.com>
2018-03-08 23:39:25 -0500

    Julia improvements.

---
+++ blackjack/julia/README.md
@@ -0,0 +1,3 @@
+```bash
+julia outcomes.jl
+```
--- blackjack/julia/outcomes.jl
+++ blackjack/julia/outcomes.jl
@@ -1,66 +1,44 @@
-#using Memoize : @memoize
-
-deck = [4,4,4,4,4,4,4,4,4,16]
-
 function partitions(cards, subtotal)
 
     m = 0
-    
     # Hit
     for i = 1:10
         if (cards[i]>0)
-            
-            cards[i] = cards[i]-1
             total = subtotal+i
-            
             if (total < 21)
-                
+                cards[i] -= 1
                 # Stand
                 m += 1
-                #println(cards, total)
                 # Hit again
                 m += partitions(cards, total)
-                
+                cards[i] += 1
             elseif (total==21)
-                
                 # Stand; hit again is an automatic bust
                 m += 1
-                #println(cards, total)
-                
             end
-                
-            cards[i] = cards[i]+1
-
         end
-
     end
-                
     return m
 
 end
 
-d = 0
+deck = [4,4,4,4,4,4,4,4,4,16]
+global d = 0
 
 for i = 1:10
 
     # Dealer showing
-
-    deck[i] = deck[i]-1
-
+    deck[i] -= 1
     p = 0
     for j = 1:10
-        deck[j] = deck[j]-1
+        deck[j] -= 1
         n = partitions(deck, j)
-        #println("Starting with ",j," ",n)
-        deck[j] = deck[j]+1
+        deck[j] += 1
         p += n
     end
-
     println("Dealer showing ", i," partitions = ",p)
     d += p
-
-    deck[i] = deck[i]+1
-
+    deck[i] += 1
 end
 
 println("Total partitions = ",d)
