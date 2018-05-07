34b848f7286383cb6bc35247eaad8547b652d8db
Christopher D. Long <galizur@gmail.com>
2018-05-07 14:55:57 -0400

    Rust update

--- blackjack/rust/outcomes.rs
+++ blackjack/rust/outcomes.rs
@@ -1,11 +1,11 @@
-fn partitions(mut cards: [usize; 10], subtotal: usize) -> i32 {
-    let mut m=0;
-    let mut total;
+fn partitions(cards: &mut [u8; 10], subtotal: u8) -> u32 {
+    let mut m=0 as u32;
+    let mut total: u8;
+    
     // Hit
-
     for i in 0..10 {
         if cards[i]>0 {
-	    total = subtotal+i+1;
+	    total = subtotal+1+(i as u8);
 	    if total < 21 {
 	        // Stand
 	        m += 1;
@@ -24,17 +24,17 @@ fn partitions(mut cards: [usize; 10], subtotal: usize) -> i32 {
     
 fn main() {
     
-    let mut deck: [usize; 10] = [4; 10];
+    let mut deck: [u8; 10] = [4; 10];
     deck[9] = 16;
     
-    let mut d=0;
+    let mut d=0 as u32;
     for i in 0..10 {
         // Dealer showing
         deck[i] -= 1;
-        let mut p = 0;
+        let mut p = 0 as u32;
         for j in 0..10 {
             deck[j] -= 1;
-            let n = partitions(deck, j+1);
+            let n = partitions(&mut deck, (j as u8)+1);
             deck[j] += 1;
             p += n;
         }
