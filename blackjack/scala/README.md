e40fcd712eeaa8fe1a459da0fa8fe47817828243
Christopher D. Long <galizur@gmail.com>
2018-05-09 02:53:05 -0400

    Scala update.

---
+++ blackjack/scala/README.md
@@ -0,0 +1,4 @@
+```bash
+scalac -opt:l:inline -opt-inline-from:** outcomes.scala
+scala outcomes
+```
--- blackjack/scala/outcomes.scala
+++ blackjack/scala/outcomes.scala
@@ -5,18 +5,18 @@ object outcomes {
     // Hit
     for (i <- 0 to 9) {
       if (cards(i)>0) {
-        cards(i) = cards(i)-1
         var total = subtotal+i+1
         if (total < 21) {
 	  // Stand
 	  m += 1
 	  // Hit again
+          cards(i) -= 1
 	  m += partitions(cards, total)
-        } else if (subtotal+i+1==21) {
+          cards(i) += 1
+        } else if (total==21) {
 	  // Stand; hit again is an automatic bust
 	  m += 1
         }
-        cards(i) = cards(i)+1
       }
     }
     return m
@@ -29,17 +29,16 @@ object outcomes {
 
     for (i <- 0 to 9) {
       // Dealer showing
-      deck(i) = deck(i)-1
+      deck(i) -= 1
       var p = 0
       for (j <- 0 to 9) {
-        deck(j) = deck(j)-1
-        var n = partitions(deck, j+1)
-        deck(j) = deck(j)+1
-        p += n
+        deck(j) -= 1
+        p += partitions(deck, j+1)
+        deck(j) += 1
       }
       println(s"Dealer showing $i partitions = $p")
       d += p
-      deck(i) = deck(i)+1
+      deck(i) += 1
     }
     println(s"Total partitions = $d")
   }
