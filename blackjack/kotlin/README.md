4b535e0cd3e4f60818b0fc78c5e9a021c6bff783
Christopher D. Long <galizur@gmail.com>
2018-05-09 03:06:30 -0400

    Kotlin improvements.

---
+++ blackjack/kotlin/README.md
@@ -0,0 +1,4 @@
+```bash
+kotlinc outcomes.kt
+kotlin OutcomesKt
+```
--- blackjack/kotlin/outcomes.kt
+++ blackjack/kotlin/outcomes.kt
@@ -3,18 +3,18 @@ fun partitions(cards: Array<Int>, subtotal: Int): Int {
 	// Hit
 	for (i in 0..9) {
 		if (cards[i]>0) {
-			cards[i] = cards[i]-1
 			var total = subtotal+i+1
 			if (total < 21) {
 				// Stand
 				m += 1
 				// Hit again
+				cards[i] -= 1
 				m += partitions(cards, total)
-			} else if (subtotal+i+1==21) {
+				cards[i] += 1
+			} else if (total==21) {
 				// Stand; hit again is an automatic bust
 				m += 1
 			}
-			cards[i] = cards[i]+1
 		}
 	}
 	return(m)
@@ -26,17 +26,16 @@ fun main(args : Array<String>) {
 
 	for (i in 0..9) {
 		// Dealer showing
-		deck[i] = deck[i]-1
+		deck[i] -= 1
 		var p = 0
 		for (j in 0..9) {
-			deck[j] = deck[j]-1
-			var n = partitions(deck, j+1)
-			deck[j] = deck[j]+1
-			p += n
+			deck[j] -= 1
+			p += partitions(deck, j+1)
+			deck[j] += 1
 		}
 		println("Dealer showing ${i} partitions = ${p}")
 		d += p
-		deck[i] = deck[i]+1
+		deck[i] += 1
 	}
 	println("Total partitions = ${d}")
 }
