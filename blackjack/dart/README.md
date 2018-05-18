08ecb33fe433dd6d1bf0765e71273f140af69193
Christopher D. Long <galizur@gmail.com>
2018-05-17 20:27:44 -0400

    Dart update.

---
+++ blackjack/dart/README.md
@@ -0,0 +1,3 @@
+```bash
+dart outcomes.dart
+```
--- blackjack/dart/outcomes.dart
+++ blackjack/dart/outcomes.dart
@@ -1,21 +1,22 @@
-int partitions(int cards, int subtotal) {
+int partitions(List cards, int subtotal) {
   int m=0;
   int total;
   // Hit
   for (int i = 0; i < 10; i++) {
     if (cards[i]>0) {
-      cards[i] = cards[i]-1;
       total = subtotal+i+1;
       if (total < 21) {
         // Stand
         m += 1;
         // Hit again
+        cards[i] = cards[i]-1;
         m += partitions(cards, total);
+        cards[i] = cards[i]+1;
       } else if (subtotal+i+1==21) {
         // Stand; hit again is an automatic bust
         m += 1;
+        break;
       }
-      cards[i] = cards[i]+1;
     }
   }
   return m;
