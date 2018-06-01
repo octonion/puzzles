3ec7c53e2d3d4de514d604ea30be01f33a2a4856
Christopher D. Long <galizur@gmail.com>
2018-06-01 17:44:37 -0400

    Small code changes.

--- blackjack/python/outcomes2.py
+++ blackjack/python/outcomes2.py
@@ -1,33 +1,26 @@
 #!/usr/bin/env python
 
-deck = ([4]*9)
-deck.append(16)
-
 def partitions(cards, subtotal):
-
     m = 0
     # Hit
     for i in xrange(10):
         if (cards[i]>0):
-            
             total = subtotal+i+1
-            
             if (total < 21):
-
-                cards[i] -= 1
                 # Stand
                 m += 1
                 # Hit again
+                cards[i] -= 1
                 m += partitions(cards, total)
                 cards[i] += 1
-                
-            elif (subtotal+i+1==21):
-                
+            elif (total==21):
                 # Stand; hit again is an automatic bust
                 m += 1
-                
+                break
     return(m)
 
+deck = ([4]*9)
+deck.append(16)
 d = 0
 
 for i in xrange(10):
@@ -39,9 +32,8 @@ for i in xrange(10):
     p = 0
     for j in xrange(10):
         deck[j] -= 1
-        n = partitions(deck, j+1)
+        p += partitions(deck, j+1)
         deck[j] += 1
-        p += n
 
     print('Dealer showing ', i,' partitions =',p)
     d += p
--- blackjack/python/outcomes3.py
+++ blackjack/python/outcomes3.py
@@ -6,24 +6,17 @@ def partitions(cards, subtotal):
     # Hit
     for i in range(0, 10):
         if (cards[i]>0):
-            
-            
             total = subtotal+i+1
-            
             if (total < 21):
-
                 cards[i] -= 1
                 # Stand
                 m += 1
                 # Hit again
                 m += partitions(cards, total)
                 cards[i] += 1
-                
-            elif (subtotal+i+1==21):
-                
+            elif (total==21):
                 # Stand; hit again is an automatic bust
                 m += 1
-                
     return(m)
 
 deck = ([4]*9)
@@ -32,21 +25,16 @@ deck.append(16)
 d = 0
 
 for i in range(0, 10):
-
     # Dealer showing
-
     deck[i] -= 1
-
     p = 0
     for j in range(0, 10):
         deck[j] -= 1
-        n = partitions(deck, j+1)
+        p += partitions(deck, j+1)
         deck[j] += 1
         p += n
-
     print('Dealer showing ', i,' partitions =',p)
     d += p
-
     deck[i] += 1
 
 print('Total partitions =',d)
