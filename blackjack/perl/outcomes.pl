ed9997e357a1d846e5ca0c57a23a5e393726ce1d
Christopher D. Long <galizur@gmail.com>
2018-06-28 14:57:41 -0400

    Perl 5 update.

--- blackjack/perl/outcomes.pl
+++ blackjack/perl/outcomes.pl
@@ -11,9 +11,9 @@ sub partitions(@$)
                 # Stand
                 $m += 1;
                 # Hit again
-		$cards[$i] = $cards[$i]-1;
+		$cards[$i] -= 1;
                 $m += partitions(\@cards,$total);
-                $cards[$i] = $cards[$i]+1;
+                $cards[$i] += 1;
             } elsif ($total==21) {
                 # Stand; hit again is an automatic bust
                 $m += 1;
@@ -29,19 +29,17 @@ my @deck = (4,4,4,4,4,4,4,4,4,16);
 
 for(my $i = 0; $i<10; $i++) {
     # Dealer showing
-    $deck[$i] = $deck[$i]-1;
+    $deck[$i] -= 1;
     my $p = 0;
     for(my $j = 0; $j<10; $j++) {
-        $deck[$j] = $deck[$j]-1;
-        my $n = partitions(\@deck,$j+1);
-        $deck[$j] = $deck[$j]+1;
-        $p += $n;
+        $deck[$j] -= 1;
+        $p += partitions(\@deck,$j+1);
+        $deck[$j] += 1;
     }
 
     print "Dealer showing ",$i," partitions = ",$p,"\n";
     $d += $p;
-
-    $deck[$i] = $deck[$i]+1;
+    $deck[$i] += 1;
 }
 
 print "Total partitions = ",$d,"\n";
