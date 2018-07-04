5386dfe86fac269f062b7f914c6b53ed160dbf08
Christopher D. Long <galizur@gmail.com>
2018-07-04 02:13:35 -0400

    pypy update.

--- blackjack/pypy/outcomes.py
+++ blackjack/pypy/outcomes.py
@@ -1,53 +1,40 @@
-deck = ([4]*9)
-deck.append(16)
-
 def partitions(cards, subtotal):
 
     m = 0
     # Hit
-    for i in range(0, 10):
+    for i in xrange(10):
         if (cards[i]>0):
-            
-            cards[i] = cards[i]-1
             total = subtotal+i+1
-            
             if (total < 21):
-                
                 # Stand
                 m += 1
-                #print(cards, total)
                 # Hit again
+                cards[i] -= 1
                 m += partitions(cards, total)
-                
-            elif (subtotal+i+1==21):
-                
+                cards[i] += 1
+            elif (total==21):
                 # Stand; hit again is an automatic bust
                 m += 1
-                #print(cards, total)
-                
-            cards[i] = cards[i]+1
-                
+                break
     return(m)
+
+deck = ([4]*9)
+deck.append(16)
 
 d = 0
 
-for i in range(0, 10):
-
+for i in xrange(10):
     # Dealer showing
-
-    deck[i] = deck[i]-1
-
+    deck[i] -= 1
     p = 0
-    for j in range(0, 10):
-        deck[j] = deck[j]-1
-        n = partitions(deck, j+1)
-        #print('Starting with ', j, n)
-        deck[j] = deck[j]+1
-        p += n
+    for j in xrange(10):
+        deck[j] -= 1
+        p += partitions(deck, j+1)
+        deck[j] += 1
 
-    print('Dealer showing ', i,' partitions =',p)
+    print 'Dealer showing ', i,' partitions =',p
     d += p
 
-    deck[i] = deck[i]+1
+    deck[i] += 1
 
-print('Total partitions =',d)
+print 'Total partitions =',d
