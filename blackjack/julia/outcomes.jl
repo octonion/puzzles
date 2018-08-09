9fa82548ca10fd32328859da6ae39bd672f5baca
Christopher D. Long <galizur@gmail.com>
2018-08-09 14:52:05 -0400

    Updated for Julia 1.0.

--- blackjack/julia/outcomes.jl
+++ blackjack/julia/outcomes.jl
@@ -1,3 +1,5 @@
+# Julia version 1.0
+
 function partitions(cards, subtotal)
 
     m = 0
@@ -6,15 +8,16 @@ function partitions(cards, subtotal)
         if (cards[i]>0)
             total = subtotal+i
             if (total < 21)
-                cards[i] -= 1
                 # Stand
                 m += 1
                 # Hit again
+                cards[i] -= 1
                 m += partitions(cards, total)
                 cards[i] += 1
             elseif (total==21)
                 # Stand; hit again is an automatic bust
                 m += 1
+                break
             end
         end
     end
@@ -22,22 +25,21 @@ function partitions(cards, subtotal)
 
 end
 
-deck = [4,4,4,4,4,4,4,4,4,16]
-global d = 0
+#deck = Array{Int,10}(4,4,4,4,4,4,4,4,4,16)
+deck = Int8[4,4,4,4,4,4,4,4,4,16]
+d = 0
 
 for i = 1:10
-
     # Dealer showing
     deck[i] -= 1
     p = 0
     for j = 1:10
         deck[j] -= 1
-        n = partitions(deck, j)
+        p += partitions(deck, j)
         deck[j] += 1
-        p += n
     end
-    println("Dealer showing ", i," partitions = ",p)
-    d += p
+    println("Dealer showing ", i-1," partitions = ",p)
+    global d += p
     deck[i] += 1
 end
 
