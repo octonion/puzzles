d8b4b06a770519e271fd785450eeed0b9f5ce8c4
Christopher D. Long <galizur@gmail.com>
2018-09-05 16:40:18 -0400

    Chudnovsky formula for pi.

--- pi/chudnovsky.jl
+++ pi/chudnovsky.jl
@@ -1,16 +1,17 @@
 # Julia version 1.0
 
-setprecision(4000)
+setprecision(40000)
 
 function term(k::Integer)
     return((-1)^k*factorial(BigInt(6*k))*(545140134*k+13591409)/( factorial(BigInt(3*k))*(factorial(BigInt(k)))^3*(BigFloat(640320))^(3*k+3/2) ))
 end
 
 p = BigFloat(term(0))
-q = p
-for i in 1:80
-    global p += term(i)
-    println(1/(12*p))
-    println("Diff = ",1/(12*p)-1/(12*q))
+q = BigFloat(0)
+for i in 1:800
     global q = p
+    global p += term(i)
 end
+println(1/(12*p))
+println("Diff = ",1/(12*p)-1/(12*q))
+
