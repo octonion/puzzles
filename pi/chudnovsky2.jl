901bc9463f1fe8afa6e28edd9f1445506fdf14ff
Christopher D. Long <galizur@gmail.com>
2018-09-05 16:52:28 -0400

    Optimized Chudnovsky formula for pi.

---
+++ pi/chudnovsky2.jl
@@ -0,0 +1,17 @@
+# Julia version 1.0
+
+setprecision(40000)
+
+function term(k::Integer)
+    return(factorial(BigInt(6*k))*(545140134*k+13591409)/( factorial(BigInt(3*k))*(factorial(BigInt(k)))^3*(-BigFloat(262537412640768000))^k ))
+end
+
+p = BigFloat(term(0))
+q = BigFloat(0)
+for i in 1:800
+    global q = p
+    global p += term(i)
+end
+println(1/((426880*sqrt(10005)*p)))
+println("Diff = ",1/((426880*sqrt(10005)*p))-1/((426880*sqrt(10005)*q)))
+
