ce5a9df539294468bc47ac5715855988b211382d
Christopher D. Long <galizur@gmail.com>
2021-06-28 22:58:18 -0400

    Python version of number construction using factorials, sqrts, floor.

---
+++ twitter/construction/factorial_sqrt.py
@@ -0,0 +1,33 @@
+from collections import deque
+from math import factorial, isqrt
+
+bound = 20000
+
+queue = deque()
+queue.append(3)
+
+found = {3}
+paths = {3 : 3}
+sqrts = {3 : 0}
+
+while (len(queue)> 0):
+    m = queue.pop()
+    n = factorial(m)
+    i = 0
+    while (n>3):
+        if (n<bound) and not(n in found):
+            found.add(n)
+            queue.appendleft(n)
+            paths[n] = m
+            sqrts[n] = i
+        n = isqrt(n)
+        i += 1
+        
+print(sorted(found))
+print()
+
+v = 9
+while not(v==3):
+    w = paths[v]
+    print("{} <- {}! + {} sqrt".format(v,w,sqrts[v]))
+    v = w
