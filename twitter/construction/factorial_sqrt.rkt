414e2909899cbff9d02e341006535a98fdfcb227
Christopher D. Long <galizur@gmail.com>
2021-07-20 10:02:40 -0400

    Racket version; slight refactoring.

--- twitter/construction/factorial_sqrt.rkt
+++ twitter/construction/factorial_sqrt.rkt
@@ -9,6 +9,28 @@
   (mpz_fac_ui n! n)
   n!)
 
+(define (shrink j i*)
+  (when (> (mpz_cmp_ui n bound) 0)
+    (mpz_sqrt j j)
+    (set-box! i* (add1 (unbox i*)))
+    (shrink j i*)
+    )
+  )
+
+(define (process k* i* m)
+  (when (> (unbox k*) 3)
+    (when (not (set-member? found (unbox k*)))
+      (set-add! found (unbox k*))
+      (enqueue! queue (unbox k*))
+      (dict-set! paths (unbox k*) m)
+      (dict-set! sqrts (unbox k*) (unbox i*))
+      )
+    (set-box! k* (integer-sqrt (unbox k*)))
+    (set-box! i* (add1 (unbox i*)))
+    (process k* i* m)
+    )
+  )
+
 (define bound 20000)
 
 (define queue (make-queue))
@@ -22,37 +44,13 @@
 (define sqrts (make-hash))
 (dict-set! sqrts 3 0)
 
-(define n 0)
+(define n (mpz))
 (for ([m (in-queue queue)])
   (set! n (gmp_fact m))
-  
   (define i* (box 0))
-  
-  (define (shrink j i*)
-    (when (> (mpz_cmp_ui n bound) 0)
-      (mpz_sqrt j j)
-      (set-box! i* (add1 (unbox i*)))
-      (shrink j i*)
-      )
-    )
   (shrink n i*)
-
   (define nn* (box (mpz_get_ui n)))
-
-  (define (process k* i*)
-    (when (> (unbox k*) 3)
-      (when (not (set-member? found (unbox k*)))
-            (set-add! found (unbox k*))
-            (enqueue! queue (unbox k*))
-            (dict-set! paths (unbox k*) m)
-            (dict-set! sqrts (unbox k*) (unbox i*))
-            )
-      (set-box! k* (integer-sqrt (unbox k*)))
-      (set-box! i* (add1 (unbox i*)))
-      (process k* i*)
-      )
-    )
-  (process nn* i*)
+  (process nn* i* m)
   )
 
 (printf "~n")
@@ -64,13 +62,14 @@
   )
 
 (printf "~n~n")
-        
+
 (define (traverse v)
   (when (> v 3)
     (printf "~a -> ~a! + ~a sqrt ~n" v (dict-ref paths v) (dict-ref sqrts v))
     (traverse (dict-ref paths v))
     )
   )
+
 (traverse 9)
 (printf "~n")
 (traverse 552)
